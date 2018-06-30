7e8f08dc01fd9e5577e87efaba53101fbc0be0c5
maksymchak <maksym4ak@gmail.com>
2018-06-30 16:32:46 +0300

    add drag and drop

--- js/setup.js
+++ js/setup.js
@@ -126,7 +126,7 @@ setupOpen.addEventListener('click', function() {
 
 setupOpen.addEventListener('keydown', function(evt) {
   if (evt.keyCode === ENTER_KEYCODE) {
-    openPopup();
+  openPopup();
   }
 });
 
@@ -136,7 +136,7 @@ setupClose.addEventListener('click', function() {
 
 setupClose.addEventListener('keydown', function(evt) {
   if (evt.keyCode === ENTER_KEYCODE) {
-    closePopup();
+  closePopup();
   }
 });
 
@@ -160,3 +160,76 @@ var isChangeColor = function (el, arr) {
 var fireballChangeColor = function (el, arr) {
   el.style.background = arr[Math.floor(Math.random() * arr.length)];
 };
+
+
+var setup = document.querySelector('.setup');
+var dialogHandle = setup.querySelector('.upload');
+
+dialogHandle.addEventListener('mousedown', function (evt) {
+  evt.preventDefault();
+
+  var startCoords = {
+    x: evt.clientX,
+    y: evt.clientY
+  };
+
+  var onMouseMove = function (moveEvt) {
+    moveEvt.preventDefault();
+
+    var shift = {
+      x: startCoords.x - moveEvt.clientX,
+      y: startCoords.y - moveEvt.clientY
+    };
+  
+    startCoords = {
+      x: moveEvt.clientX,
+      y: moveEvt.clientY
+    };
+
+    setup.style.top = (setup.offsetTop - shift.y) + 'px';
+    setup.style.left = (setup.offsetLeft - shift.x) + 'px';
+  };
+
+  var onMouseUp = function (upEvt) {
+    upEvt.preventDefault();
+
+    document.removeEventListener('mousemove', onMouseMove);
+    document.removeEventListener('mouseup', onMouseUp);
+  };
+
+  document.addEventListener('mousemove', onMouseMove);
+  document.addEventListener('mouseup', onMouseUp);
+});
+
+var shopElement = document.querySelector('.setup-artifacts-shop');
+var draggedItem = null;
+
+shopElement.addEventListener('dragstart', function (evt) {
+  if (evt.target.tagName.toLowerCase() === 'img') {
+    draggedItem = evt.target;
+    evt.dataTransfer.setData('text/plan', evt.target.alt);
+  }
+});
+
+var artifactsElement = document.querySelector('.setup-artifacts');
+
+artifactsElement.addEventListener('dragover', function (evt) {
+  evt.preventDefault();
+  return.false;
+});
+
+artifactsElement.addEventListener('drop', function (evt) {
+  evt.target.style.backgroundColor = '';
+  evt.target.appendChild(draggedItem);
+  evt.preventDefault();
+});
+
+artifactsElement.addEventListener('dragenter', function (evt) {
+  evt.target.style.backgroundColor = 'yellow';
+  evt.preventDefault();
+});
+
+artifactsElement.addEventListener('dragleave', function (evt) {
+  evt.target.style.backgroundColor = '';
+  evt.preventDefault();
+});
